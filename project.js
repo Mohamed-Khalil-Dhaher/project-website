9f52082b1080c867ff5c912a9000bab236c807d7
Mohamed-Khalil-Dhaher <dhaherkhalil91@gmail.com>
2020-08-18 13:03:14 +0200

    advenced

---
+++ project.js
@@ -0,0 +1,16 @@
+	function myFunction() {
+  document.getElementById("list").innerHTML = "my installion";
+}
+
+var x = document.getElementById("myMoto"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+$("button").click(function(){ 
+$("iframe").show()
+})
+
+$("topnav").click(function(){
+	$("img").show()
+})
